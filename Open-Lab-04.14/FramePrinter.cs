3d8460c350b76d60a1ed2c7fdcf9092c67640a38
Roman <vojtus.roman@gmail.com>
2022-12-20 00:23:20 +0100

    ...

--- Open-Lab-04.14/FramePrinter.cs
+++ Open-Lab-04.14/FramePrinter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Open_Lab_04._14
 {
@@ -6,7 +7,28 @@ namespace Open_Lab_04._14
     {
         public void Print(string[] strings)
         {
-            throw new NotImplementedException();
+        int max = 0;
+        for (int i = 0; i < strings.Length; i++)
+            {
+                if (max < strings[i].Length)
+                {
+                    max = strings[i].Length;
+                }
+            }
+            int width = max+4;
+            Console.WriteLine(String.Concat(Enumerable.Repeat("*", width)));
+            for (int i = 0; i < strings.Length; i++)
+            {
+                Console.Write("* ");
+                Console.Write(strings[i]);
+                for (int j = 0; j <max-strings[i].Length; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(" *");
+            }
+            Console.WriteLine(String.Concat(Enumerable.Repeat("*", width)));
+
         }
     }
 }
